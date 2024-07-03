284d6bd16643f8d4970724fed0f7d4ddcc8e5ed9
thdfw <th.defauw@gmail.com>
2024-07-02 17:32:54 -0700

    Small bugs solved

--- fmu_simulations/fmu_new/fmu_new.py
+++ fmu_simulations/fmu_new/fmu_new.py
@@ -27,14 +27,14 @@ PRINT = True
 # --------------------------
 
 # Yearly data
-df = pd.read_excel(os.getcwd()+'/data/gridworks_yearly_data.xlsx', header=3, index_col = 0)
-df.index = pd.to_datetime(df.index)
-df.index.name = None
-df['elec'] = df['Total Delivered Energy Cost ($/MWh)']
-df['load'] = df['House Power Required AvgKw'] * 0.8
-df['T_OA'] = df['Outside Temp F']
-df['T_OA'] = df['T_OA'].apply(lambda x: round(5/9 * (x-32),2))
-df = df[['elec', 'load', 'T_OA']]
+df_yearly = pd.read_excel(os.getcwd()+'/data/gridworks_yearly_data.xlsx', header=3, index_col = 0)
+df_yearly.index = pd.to_datetime(df_yearly.index)
+df_yearly.index.name = None
+df_yearly['elec'] = df_yearly['Total Delivered Energy Cost ($/MWh)']
+df_yearly['load'] = df_yearly['House Power Required AvgKw'] * 0.8
+df_yearly['T_OA'] = df_yearly['Outside Temp F']
+df_yearly['T_OA'] = df_yearly['T_OA'].apply(lambda x: round(5/9 * (x-32),2))
+df_yearly = df_yearly[['elec', 'load', 'T_OA']]
 
 # Get COP regression as a function of T_OA
 T_OA_table = list(range(-15,40,5))
@@ -65,15 +65,14 @@ def simulate(delta_HP, T_sup_HP, hour):
         'HeatPumpWaterTempSetpoint': T_sup_HP + 273,
         'HeatPumpOnOff': delta_HP,
         'HeatPumpMode': True,
-        'OutdoorAirTemperature': df.T_OA[hour] + 273,
-        'ZoneHeatingLoad': df.load[hour] * 1000,
+        'OutdoorAirTemperature': df_yearly.T_OA[hour] + 273,
+        'ZoneHeatingLoad': df_yearly.load[hour] * 1000,
     }  
 
     # Buildig the FMU input
     input_names = list(inputs_dict)
     inputs_array = np.array([[minute]+list(inputs_dict.values()) for minute in range(start_time,final_time+60,60)])
     inputs = (input_names, inputs_array)
-    if PRINT: print(inputs)
     
     # Simulation options
     opts = model.simulate_options()
@@ -89,12 +88,12 @@ def simulate(delta_HP, T_sup_HP, hour):
 
     # The simulation outputs a .mat file with results, convert it to csv
     time.sleep(1)
-    command = f"python mat_to_csv.py {fmuName.replace(".fmu","")+'_result.mat'} > prints.txt"
+    command = f"python mat_to_csv.py {fmuNameNoSuffix+'_result.mat'} > prints.txt"
     subprocess.call(command, shell=True)
 
     # Read and return the results file as a dataframe
-    results_df = pd.read_csv(fmuName.replace(".fmu","")+'_result.csv').drop('Unnamed: 0', axis=1)
-    return results_df
+    df = pd.read_csv(fmuNameNoSuffix+'_result.csv').drop('Unnamed: 0', axis=1)
+    return df
 
 # --------------------------
 # Initialize
@@ -112,6 +111,7 @@ total_cost = 0
 
 # Load FMU
 fmuName = 'R32SimpleHpTesDummyZone20kWh.fmu'
+fmuNameNoSuffix = fmuName.replace(".fmu","")
 model = pyfmi.load_fmu(fmuName)
 model.set('phaseChangeBattery58.Design.Tes_nominal', storage_capacity*3600000)
 if PRINT: print(f"Model {fmuName} was loaded.\n")
@@ -127,10 +127,10 @@ for hour in range(24):
     parameters = { 
         'horizon': 24,
 
-        'elec_costs': list(df.elec[hour:hour+24]),
+        'elec_costs': list(df_yearly.elec[hour:hour+24]),
     
         'load': {'type': 'hourly', 
-                'value': list(df.load[hour:hour+24])},
+                'value': list(df_yearly.load[hour:hour+24])},
 
         'control': {'type': 'range',
                     'max': [hp_capacity]*24,
@@ -143,8 +143,10 @@ for hour in range(24):
                         'quiet_hours': False},
 
         'hardware': {'heatpump': True,
-                     'COP': [get_COP(x) for x in df.T_OA[hour:hour+24]]}
+                     'COP': [get_COP(x) for x in df_yearly.T_OA[hour:hour+24]]}
     }
+
+    print(parameters)
 
     # Get the controls from the generic algorithm
     Q_HP = generic(parameters)
@@ -157,10 +159,26 @@ for hour in range(24):
     # Send commands to FMU and obtain simulation results
     df = simulate(delta_HP, T_sup_HP, hour)
     df = df.drop(df.index[-1])
+
+    # Convert SOC to kWh
+    df['SOC'] = df['SOC'] * storage_capacity
+    df['SOC'] = df['SOC'].round(2)
+    # Compute real Q_HP
+    df['Q_HP'] = df['HeatPumpOnOff'] * 0.29 * 4187 * (df['T_HP_sup'] - df['T_HP_ret']) / 1000
+    df['Q_HP'] = df['Q_HP'].round(2)
+    df['HeatPumpOnOff'] = df['HeatPumpOnOff'].round()
+    df['T_HP_ret'] = df['T_HP_ret'].round(1)
+    df['T_HP_sup'] = df['T_HP_sup'].round(1)
+    # Add inputs to df
+    df['INPUT_delta_HP'] = [delta_HP for _ in range(60)]
+    df['INPUT_T_HP_sup_setpoint'] = [T_sup_HP+273 for _ in range(60)]
+    # Compute expected Q_HP
+    df['Q_HP_expected'] = df['INPUT_delta_HP'] * 0.29 * 4187 * (df['INPUT_T_HP_sup_setpoint'] - T_HP_in - 273) / 1000
+    df['Q_HP_expected'] = df['Q_HP_expected'].round(2)
     print(df)
 
-    # Convert SoC to kWh and update
-    soc = round(df['SOC'].iloc[-1] * storage_capacity, 2)
+    # Update SoC
+    soc = df['SOC'].iloc[-1]
 
     # Cost of the last hour
     cost = Q_HP[0] * parameters['elec_costs'][0] / parameters['hardware']['COP'][0]
@@ -169,8 +187,8 @@ for hour in range(24):
     print(f'Hour {hour}, Q_HP {Q_HP[0]}, cost {round(cost,3)}, SoC {round(soc,2)}')
 
 # Plot the whole operation
-parameters['elec_costs'] = list(df.elec[:24])
-parameters['load']['value'] = list(df.load[:24])
+parameters['elec_costs'] = list(df_yearly.elec[:24])
+parameters['load']['value'] = list(df_yearly.load[:24])
 parameters['constraints']['initial_soc'] = soc_0
 print(f"\nThe control sequence is:\n{final_Q_HP_sequence} kWh\n")
 iteration_plot({'control': final_Q_HP_sequence}, parameters)
